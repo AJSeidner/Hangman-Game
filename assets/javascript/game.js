d77501b16cdc2bd8138d309378ca15b31b0b6632
AJSeidner <AJSeidner@gmail.com>
2020-11-12 09:49:40 -0600

    deleted files, modified javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,46 @@
 //global variables
-//arrays and variables for holding data
 
-var wordOptions = ["tiny", "doug", "rugrats"];
-var selectedWord = "";
-var lettersInWord = [];
+
+//array of word options
+var hangmanWords = ["goliath", "gargoyles", "xanatos", "demona", "brooklyn", "lexington", "broadway", "elisa", "bronx", "hudson"];
+
+//following global variables will be updated based off gameplay, initial setting defined.
+//word that will be chosen from array
+var gameWord = "";
+//array for letter of game word
+var lettersGameWord = [];
+//number of blank spaces shown based on lettersGameWord
 var numBlanks = 0;
-var blanksAndSuccesses = [];
-var wrongLetters = [];
-var letterGuessed = "";
+//actual array for blanks and correct letter guesses
+var blanksLetters = [];
+//array for incorrect guesses
+var wrongGuesses = [];
 
-var winCount = 0;
-var lossCount = 0;
-var guessesLeft = 9;
+//counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
 
-//functions
+
+//game functions
+
+//startGame()
+//function for starting/re-starting game
 
 function startGame() {
-    selectedWord = wordOptions[Math.floor(Math.random()* wordOptions.length)];
-    lettersInWord = selectedWord.length;
-    numBlanks = lettersInWord.length;
-
-    guessesleft = 9;
-    wrongLetters = [];
-    blanksAndSuccesses = [];
-
-    //populate blanksAndSuccesses
-
-    for (var i = 0; i<numBlanks; i++){
-        blanksAndSuccesses[i].push("_");
-    }
-document.getElementById("gameWord").innerHTML = blanksAndSuccesses.join(" ");
-document.getElementById("guesses").innerHTML = guessesLeft;
-document.getElementById("wins").innerHTML = winCount;
-document.getElementById("losses").innerHTML = lossCount;
-}
-
-function checkLetters(letter){
-
-    var isLetterInWord = false;
-
-    for (var i=0; i<numBlanks; i++){
-        if(selectedWord[i] == letter){
-            isLetterInWord = true;
-            console.log("yay");
-        }
-    }
-
-    for (var i=0; i<numBlanks; i++){
-        if(selectedWord[i] == letter) {
-            blanksAndSuccesses[i] = letter;
-        }
-
-    else {
-        wrongLetters.push(letter);
-        guessesLeft--;
-        console.log("boo");
-        }
-    }
-}
-
-function roundComplete(){
-
-    document.getElementById("guesses").innerHTML = guessesLeft;
-    document.getElementById("gameWord").innerHTML =blanksAndSuccesses.toString();
-    document.getElementById("letterGuessed").innerHTML =wrongLetters.toString();
-    if (lettersInWord.toString() == blanksAndSuccesses.toString()){
-        winCount++;
-        alert("You Won!");
-
-        document.getElementById("wins").innerHTML =winCount;
-
-        startGame();
-    }
-    else if (guessesLeft== 0) {
-        lossCount++;
-        alert("You lost!");
-        document.getElementById("losses").innerHTML =lossCount;
-
-        startGame();
-
-    }
-
-}
+    // reset numGuesses
+    numGuesses = 9;
+    // choosing gameWords
+    gameWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
+    // break game word down into individual letters using .split method
+    lettersGameWord = gameWord.split("");
+    // getting number of blanks from by counting letters using .length
+    numBlanks = lettersGameWord.length;
+    // test
+    console.log(gameWord);
+};
 
 
+// code to run game
 
 startGame();
-
-document.onkeyup = function(event) {
-
-    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    checkLetters(letterGuessed);
-    roundComplete();
-    console.log(letterGuessed);
-}
-
-
--- coinflip.js
+++
@@ -1,4 +0,0 @@
-var wins = 0;
-var losses = 0;
-var headsChosen = 0;
-var tailsChosen = 0;
